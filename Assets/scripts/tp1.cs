8648418704bb92f518acdfa2696c0e31a5d6715b
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-04-07 08:34:52 -0300

    exercise 1 done

---
+++ Assets/scripts/tp1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tp1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("los dos valores son iguales");
+        }
+
+        else
+        {
+            Debug.Log(" los valores no son iguales ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
