0cb744d56b24f7f6df1cd48ddf4893f9a5ba034e
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-04-07 08:43:19 -0300

    exercise 2 done

--- Assets/scripts/tp2.cs
+++ Assets/scripts/tp2.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class tp2 : MonoBehaviour
 {
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float monto;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      float suma = precio1 + precio2 + precio3;
+
+        if (suma > monto)
+        {
+            Debug.Log("el monto es menor al precio de los tres productos");
+        }
+        else if (suma == monto)
+        {
+            Debug.Log("el monto es igual a el precio de los tres productos");
+        }
+
+        else
+        {
+            Debug.Log(" el monto es mayor al precio de los tres productos");
+        }
+
+       float resto = monto - suma;
+
+        if (resto < 0)
+        {
+            Debug.Log("no a sobrado dinero ");
+        }
+
+        else
+        {
+            Debug.Log(" sobraron " + resto + "pesos ");
+        } 
+
+
+        }
     }
 
     // Update is called once per frame
